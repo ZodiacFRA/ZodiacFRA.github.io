4f3c8912dba00bcd5e5afad9ef58bcefc8bed7df
ZodiacFRA <jean-baptiste.dupuy@epitech.eu>
2022-06-06 19:58:21 +0200

    MAJ

--- template_system/templates/footer.py
+++
@@ -1,8 +0,0 @@
-MAIN_FOOTER = """<!--------------------------------------------------------------------------- EOF -->
-    <h2 style="text-align: right;"><a href='./older_works.html'>Older ⭢</a></h2>
-    <script src="https://zodiacfra.github.io/ZodiacFRA/scripts/slideshow.js"></script>
-</body>"""
-
-OLDER_WORKS_FOOTER = """<!--------------------------------------------------------------------------- EOF -->
-    <script src="https://zodiacfra.github.io/ZodiacFRA/scripts/slideshow.js"></script>
-</body>"""
--- template_system/templates/misc.py
+++
@@ -1,6 +0,0 @@
-CODE = """<h2>“Talk is cheap. Show me the code.”</h2>
-<h3>Linus Torvalds</h3>
-<a href="https://github.com/ZodiacFRA" class="header_block">
-    <img class="header_icon" style="height: auto; width: 36px; margin-top: 10px;" src="{}/images/misc/github-svgrepo-com.svg">
-    <h2 class="header_link">Github</h2>
-</a>"""
